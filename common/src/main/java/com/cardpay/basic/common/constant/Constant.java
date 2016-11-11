30c8d12a813a5e38c5e1c96998ce4c642cf3c958
johnmyqin <896245907@qq.com>
2016-11-11 17:38:19 +0800

    提交基础代码和功能

---
+++ common/src/main/java/com/cardpay/basic/common/constant/Constant.java
@@ -0,0 +1,76 @@
+package com.cardpay.basic.common.constant;
+
+/**
+ * 用于定义全局常量 .
+ *
+ * @author licho
+ */
+public class Constant {
+    //角色名定义
+    /**
+     * 管理员
+     */
+    public static final String ROLE_ADMIN = "admin";
+    /**
+     * 客户经理
+     */
+    public static final String ROLE_MANAGER = "manager";
+    /**
+     * 专家
+     */
+    public static final String ROLE_EXPERT = "expert";
+    /**
+     * 管理岗
+     */
+    public static final String ROLE_LEADER = "leader";
+
+    /**
+     * 初始密码
+     */
+    public static final String INIT_PASSWORD = "qkjr123";
+
+    /**
+     * 分页json编码格式
+     */
+    public static final String APPLICATION_JSON = "application/json;charset=utf-8";
+
+    /**
+     * 进件UUID基数
+     */
+    public static final String UUID = "5188290547340000";
+
+    /**
+     * 短信或邮箱验证码超时时间
+     */
+    public static final int TIME_OUT = 300;
+
+    /**
+     * email正则表达式
+     */
+    public static final String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|_|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+
+    /**
+     * 手机号正则表达式
+     */
+    public static final String REGEX_PHONE = "^1(3[0-9]|5[0-35-9]|8[025-9])\\d{8}$";
+
+    /**
+     * 登陆锁定时间(6小时)
+     */
+    public static final int LOGIN_TIMEOUT = 6 * 60 * 60;
+
+    /**
+     * 登陆密码输入错误次数超过该设置值后将锁定
+     */
+    public static final int LOCK_COUNT = 6;
+
+    /**
+     * 登陆密码输入错误次数超过该设置值后将弹出验证码
+     */
+    public static final int CAPTCHA_COUNT = 3;
+
+    /**
+     * 表单重复提交，表单重复提交session key值，可以通过该值获取token来判断是否重复提交
+     */
+    public static final String TOKEN_REPEAT_SESSION_KEY = "token_repeat_session_key";
+}
---
+++ common/src/main/java/com/cardpay/basic/common/enums/ResultEnum.java
@@ -0,0 +1,107 @@
+package com.cardpay.basic.common.enums;
+
+/**
+ * APi返回状态定义
+ * @author rankai .
+ */
+public enum ResultEnum {
+    /**
+     * api接口成功
+     */
+    SUCCESS(200),
+    /**
+     * 没有权限
+     */
+    NO_AUTHORITY(401),
+    /**
+     * 未找到
+     */
+    NOT_FOUND(404),
+    /**
+     * 服务器错误
+     */
+    SERVICE_ERROR(500),
+    /**
+     * token失效
+     */
+    TOKEN_ERROR(5001),
+    /**
+     * 参数错误
+     */
+    PARAM_ERROR(5002),
+    /**
+     * 用户已存在
+     */
+    USER_EXIST(5003),
+    /**
+     * 用户或者密码错误
+     */
+    USER_PWD_ERROR(5004),
+    /**
+     * 账号未激活
+     */
+    NO_ACTIVATION_EXCEPTION(5005),
+    /**
+     * 账号不存在
+     */
+    UNKNOWN_ACCOUNT(5006),
+    /**
+     * 账号被锁定
+     */
+    LOCKED_ACCOUNT(5007),
+    /**
+     * 登陆密码错误次数过多
+     */
+    TOO_MANY_ERRORS(5008),
+    /**
+     * 验证码错误
+     */
+    CAPTCHA_ERROR(5009),
+    /**
+     * 验证码超时
+     */
+    CAPTCHA_TIMEOUT(5010),
+    /**
+     * 客户输入验证码为空
+     */
+    CAPTCHA_NULL(5010),
+    /**
+     * 没有登陆
+     */
+    NO_LOGIN(5011),
+    /**
+     * 没有权限
+     */
+    NO_PERMITTION(5012),
+    /**
+     * 不是合法的邮箱或手机号
+     */
+    NUMBER_ERROR(5013),
+    /**
+     * 填写的绑定邮箱错误
+     */
+    BOUND_MAILBOX_ERROR(5014),
+    /**
+     * 填入的绑定手机号错误
+     */
+    BOUND_PHONE_ERROR(5015),
+    /**
+     * 修改密码:原密码错误
+     */
+    OLD_PASSWORD_ERROR(5016);
+
+    private int value; // 自定义数据域，private为了封装。
+
+    ResultEnum(int val) {
+        this.value = val;
+    }
+
+    /**
+     * 获取对应的值
+     *
+     * @return 对应值value
+     */
+    public int getValue() {
+        return value;
+    }
+}
