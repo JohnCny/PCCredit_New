71ebf1749d759ec0168aad305b886f15a58eb74b
chenkai <436071185@qq.com>
2016-11-15 14:47:45 +0800

    修改swagger配置

--- web/src/main/java/com/cardpay/controller/base/TestController.java
+++ web/src/main/java/com/cardpay/controller/base/TestController.java
@@ -1,23 +1,25 @@
 package com.cardpay.controller.base;
 
 import com.cardpay.mgt.model.TModel;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by yanwe on 2016/11/14.
+ * Created by chenkai on 2016/11/15.
  */
+@Api(description = "测试类")
 @RestController
 @RequestMapping("/test")
 public class TestController extends BaseController<TModel> {
-    @ApiOperation(value = "测试接口3", notes = "接受对象", produces = "application/json",  httpMethod = "POST")
-    @ApiResponses(value = {@ApiResponse(code = 405, message = "invalid input")})
-    @RequestMapping("/a")
-    public String test(){
+    @ApiOperation(value = "测试接口", notes = "测试spring-fox",  httpMethod = "POST", produces = "application/json")
+    @ApiResponses(value = {@ApiResponse(code = 405, message = "请求类型异常")})
+    @RequestMapping(value = "a" ,method = RequestMethod.GET)
+    public String test(@ApiParam(value = "测试数据", required = true) @RequestParam(value="test") String test){
         return "dist/index";
     }
 }
