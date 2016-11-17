7451952463b269167dbc2b2f0b99bc3234b8590e
yanweichen <505717760@qq.com>
2016-11-17 15:05:40 +0800

    feat:添加单元测试环境

--- web/src/main/java/com/cardpay/controller/test/TestController.java
+++ web/src/main/java/com/cardpay/controller/test/TestController.java
@@ -1,9 +1,8 @@
-package com.cardpay.controller.base;
+package com.cardpay.controller.test;
 
+import com.cardpay.controller.base.BaseController;
 import com.cardpay.mgt.model.TModel;
 import io.swagger.annotations.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api(description = "测试类")
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/api/test")
 public class TestController extends BaseController<TModel> {
+
     @ApiOperation(value = "测试接口", notes = "测试spring-fox",  httpMethod = "POST", produces = "application/json")
     @ApiResponses(value = {@ApiResponse(code = 405, message = "请求类型异常")})
     @RequestMapping(value = "a" ,method = RequestMethod.GET)
     public String test(@ApiParam(value = "测试数据", required = true) @RequestParam(value="test") String test){
         return "dist/index";
     }
+    
 }
