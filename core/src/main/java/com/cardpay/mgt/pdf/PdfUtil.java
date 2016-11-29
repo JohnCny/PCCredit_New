0f360aeefa45abad1fb291af15254c4ab5a94ee5
yanweichen <505717760@qq.com>
2016-11-29 16:20:40 +0800

    feat:修改解析pdf,修改单元测试

--- core/src/main/java/com/cardpay/mgt/pdf/PdfUtil.java
+++ core/src/main/java/com/cardpay/mgt/pdf/PdfUtil.java
@@ -10,16 +10,27 @@ import java.io.*;
  */
 public class PdfUtil {
 
-    public void readPDF() throws IOException {
-        FileInputStream fis   =   new   FileInputStream("F:\\task\\lerman-atem2001.pdf");
-        BufferedWriter writer = new BufferedWriter(new FileWriter("F:\\task\\pdf_change.txt"));
-        PDFParser p   =   new   PDFParser(fis);
-        p.parse();
-        PDFTextStripper ts   =   new   PDFTextStripper();
-        String   s   =   ts.getText(p.getPDDocument());
-        writer.write(s);
-        System.out.println(s);
-        fis.close();
-        writer.close();
+    /**
+     * 读取PDF为字符串
+     * @param fis PDF文件流
+     * @return
+     */
+    public static String readPDF(FileInputStream fis){
+        try {
+            PDFParser pdfParser = new PDFParser(fis);
+            pdfParser.parse();
+            PDFTextStripper ts = new PDFTextStripper();
+            String result = ts.getText(pdfParser.getPDDocument());
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 }
