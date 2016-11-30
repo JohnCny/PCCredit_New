008f110b9d8837df50a7ddce2fdd4a010123a0ab
chenkai <436071185@qq.com>
2016-11-30 10:43:38 +0800

    天剑单元测试

---
+++ core/src/test/java/com/cardpay/mgt/file/dao/TFileMapperTest.java
@@ -0,0 +1,52 @@
+package com.cardpay.mgt.file.dao;
+
+import com.cardpay.mgt.file.model.TFile;
+import com.cardpay.util.TestEnv;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by chenkai on 2016/11/30.
+ */
+public class TFileMapperTest extends TestEnv{
+    @Autowired
+    private TFileMapper tFileDao;
+
+    @Test
+    public void batchInsert() throws Exception {
+        List<TFile> list = new ArrayList<>();
+        TFile tFile1 = new TFile();
+        tFile1.setFileName("1");
+        tFile1.setFastName("1");
+        tFile1.setGroupName("1");
+        tFile1.setModifiedBy("1");
+        tFile1.setCreatedAt(new Date());
+        tFile1.setCreatedAt(new Date());
+        tFile1.setId(1);
+        tFile1.setImageType("1");
+        tFile1.setRemark("1");
+        tFile1.setCreatedBy("1");
+        TFile tFile2 = new TFile();
+        tFile2.setFileName("2");
+        tFile2.setFastName("2");
+        tFile2.setGroupName("2");
+        tFile2.setModifiedBy("2");
+        tFile2.setCreatedAt(new Date());
+        tFile2.setCreatedAt(new Date());
+        tFile2.setId(2);
+        tFile2.setImageType("2");
+        tFile2.setRemark("2");
+        tFile2.setCreatedBy("2");
+        list.add(tFile1);
+        list.add(tFile2);
+        int flag = tFileDao.batchInsert(list);
+        assertEquals(flag, 1);
+    }
+
+}
