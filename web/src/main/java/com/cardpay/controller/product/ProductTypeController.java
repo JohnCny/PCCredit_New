6110c1afe7afd0f9f02f4e80fb1d8fdf50491d6e
chenkai <436071185@qq.com>
2016-12-13 13:49:53 +0800

    添加产品类型表

---
+++ web/src/main/java/com/cardpay/controller/product/ProductTypeController.java
@@ -0,0 +1,11 @@
+package com.cardpay.controller.product;
+
+import com.cardpay.controller.base.BaseController;
+import com.cardpay.mgt.product.model.TProductType;
+
+/**
+ * 产品类型Controller
+ * Created by chenkai on 2016/12/13.
+ */
+public class ProductTypeController extends BaseController<TProductType, Integer> {
+}
