d0f5ed92deae946ec8c2709b9fca5d4d538378c9
rankai <root>
2016-12-23 14:12:03 +0800

    补充提交

--- web/src/main/java/com/cardpay/controller/user/UserController.java
+++ web/src/main/java/com/cardpay/controller/user/UserController.java
@@ -129,8 +129,8 @@ public class UserController extends BaseController<User> {
      */
     @ApiResponses(value = {@ApiResponse(code = 405, message = "请求类型异常"), @ApiResponse(code = 500, message = "服务器异常")})
     @ApiOperation(value = "用户角色页面跳转", httpMethod = "GET")
-    @RequestMapping(value = "/{userId}/{roleId}", method = RequestMethod.GET)
-    public ResultTo updateUserRole(@PathVariable("userId") Integer userId, @PathVariable("roleId") Integer roleId) {
+    @RequestMapping(value = "/{userId}/updateUserRole", method = RequestMethod.GET)
+    public ResultTo updateUserRole(@PathVariable("userId") Integer userId, @RequestParam("roleId") Integer roleId) {
         UserRole userRole = new UserRole();
         userRole.setRoleId(roleId);
         userRole.setUserId(userId);
