4ea436360c8b0b543522cbfb6888cd64266056b4
chenkai <436071185@qq.com>
2017-01-06 09:32:18 +0800

    feat(dao,service) -添加团队基本文件 fix(controller) -修复客户维护信息添加异常

---
+++ core/src/main/java/com/cardpay/mgt/team/service/TeamService.java
@@ -0,0 +1,13 @@
+package com.cardpay.mgt.team.service;
+
+import com.cardpay.basic.base.service.BaseService;
+import com.cardpay.mgt.team.model.Team;
+
+/**
+ * 团队表Service
+ *
+ * @author chenkai
+ *         createTime 2017-01-2017/1/6 9:28
+ */
+public interface TeamService extends BaseService<Team> {
+}
