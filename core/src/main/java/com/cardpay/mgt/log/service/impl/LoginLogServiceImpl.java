d8128c518eea511c828386378373d2779cd3c875
rankai <root>
2017-01-10 16:12:54 +0800

    增加用户文件上传

--- core/src/main/java/com/cardpay/mgt/log/service/impl/LoginLogServiceImpl.java
+++ core/src/main/java/com/cardpay/mgt/log/service/impl/LoginLogServiceImpl.java
@@ -5,6 +5,7 @@ import com.cardpay.basic.base.service.impl.BaseServiceImpl;
 import com.cardpay.mgt.log.model.LoginLog;
 import com.cardpay.mgt.log.service.LoginLogService;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.formula.functions.FinanceLib;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.WebRequest;
 import tk.mybatis.mapper.entity.Example;
@@ -19,6 +20,12 @@ import java.util.Map;
  */
 @Service
 public class LoginLogServiceImpl extends BaseServiceImpl<LoginLog> implements LoginLogService {
+
+    private static final String PAGELIST_PRAM_LOGINACCOUNT = "loginAccount";
+
+    private static final String PAGELIST_PRAM_LOGINOPERATION = "loginOperation";
+
+    private static final String PAGELIST_PRAM_LOGINTIME = "loginTime";
 
     @Override
     public Example pageList(WebRequest request) {
@@ -42,14 +49,14 @@ public class LoginLogServiceImpl extends BaseServiceImpl<LoginLog> implements Lo
         }
         if (map != null) {
             Example.Criteria criteria = example.createCriteria();
-            if (StringUtils.isNotBlank(map.get("loginAccount"))) {
-                criteria.andEqualTo("LOGIN_ACCOUNT", map.get("loginAccount"));
+            if (StringUtils.isNotBlank(map.get(PAGELIST_PRAM_LOGINACCOUNT))) {
+                criteria.andEqualTo("LOGIN_ACCOUNT", map.get(PAGELIST_PRAM_LOGINACCOUNT));
             }
-            if (StringUtils.isNotBlank(map.get("loginOperation"))) {
-                criteria.andEqualTo("LOGIN_OPERATION", map.get("loginOperation"));
+            if (StringUtils.isNotBlank(map.get(PAGELIST_PRAM_LOGINOPERATION))) {
+                criteria.andEqualTo("LOGIN_OPERATION", map.get(PAGELIST_PRAM_LOGINOPERATION));
             }
-            if (StringUtils.isNotBlank(map.get("loginTime"))) {
-                criteria.andCondition("to_char(LOGIN_TIME,'yyyy-mm-dd')=", map.get("loginTime"));
+            if (StringUtils.isNotBlank(map.get(PAGELIST_PRAM_LOGINTIME))) {
+                criteria.andCondition("to_char(LOGIN_TIME,'yyyy-mm-dd')=", map.get(PAGELIST_PRAM_LOGINTIME));
             }
         }
         return example;
--- web/src/main/java/com/cardpay/controller/user/UserController.java
+++ web/src/main/java/com/cardpay/controller/user/UserController.java
@@ -19,6 +19,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
@@ -143,7 +144,7 @@ public class UserController extends BaseController<User> {
     @PutMapping
     @ApiOperation(value = "编辑用户实现", httpMethod = "PUT")
     public ResultTo updateUser(User user, BindingResult result, @RequestParam(value = "orgId", required = false) String orgId,
-                               @RequestParam(value = "roleId", required = false) String roleId) {
+                               @RequestParam(value = "roleId", required = false) String roleId, MultipartFile file) {
         LogTemplate.debug(this.getClass(), "orgId", orgId);
         LogTemplate.debug(this.getClass(), "roleId", roleId);
         Map<String, String> map = new HashedMap();
@@ -163,7 +164,7 @@ public class UserController extends BaseController<User> {
                 return new ResultTo(ResultEnum.PARAM_ERROR);
             }
         }
-        if (userService.updateUser(user, orgIds, roleIds)) {
+        if (userService.updateUser(user, orgIds, roleIds, file)) {
             return new ResultTo();
         }
         return new ResultTo(ResultEnum.OPERATION_FAILED);
