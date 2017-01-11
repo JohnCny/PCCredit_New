91e93a2051dbc15cef8773552cb048264a387c55
yanweichen <505717760@qq.com>
2017-01-11 09:16:29 +0800

    refactor:重构进件

---
+++ core/src/main/java/com/cardpay/mgt/application/ipc/universal/dao/TApplicationTemplateMapper.java
@@ -0,0 +1,7 @@
+package com.cardpay.mgt.application.ipc.universal.dao;
+
+import com.cardpay.basic.base.mapper.BasicMapper;
+import com.cardpay.mgt.application.ipc.universal.model.TApplicationTemplate;
+
+public interface TApplicationTemplateMapper extends BasicMapper<TApplicationTemplate> {
+}
