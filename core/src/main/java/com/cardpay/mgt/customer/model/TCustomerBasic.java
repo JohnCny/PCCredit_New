248320100ba2a66bbe6600c7072c3d8a5461e389
chenkai <436071185@qq.com>
2017-01-12 15:13:13 +0800

    fix(controller) -给客户列表添加删除标记

--- core/src/main/java/com/cardpay/mgt/customer/model/TCustomerBasic.java
+++ core/src/main/java/com/cardpay/mgt/customer/model/TCustomerBasic.java
@@ -133,6 +133,17 @@ public class TCustomerBasic extends GenericEntity<Integer> {
     @ApiModelProperty(value = "(0正常，1 高风险用户  2 黑名单用户  3 高风险转黑名单审核  4 黑名单转出  5 禁用客户  6 客户移交中)", required = true)
     private Integer customerStatus;
 
+    @Column
+    private Integer ifDel;
+
+    public Integer getIfDel() {
+        return ifDel;
+    }
+
+    public void setIfDel(Integer ifDel) {
+        this.ifDel = ifDel;
+    }
+
     /**
      * 获取客户id(需要生成规则生成)
      *
