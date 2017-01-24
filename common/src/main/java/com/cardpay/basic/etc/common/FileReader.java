27c63fedfb8277d10d1f39f66cd294863843b5fa
johnmyqin <896245907@qq.com>
2017-01-24 13:59:16 +0800

    提交txt文件读写功能

--- common/src/main/java/com/cardpay/basic/etc/common/FileReader.java
+++ common/src/main/java/com/cardpay/basic/etc/common/FileReader.java
@@ -2,11 +2,9 @@ package com.cardpay.basic.etc.common;
 
 import com.cardpay.basic.common.log.LogTemplate;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
+import java.util.List;
 
 /**
  * @author johnmyiqn 2017/1/23 15:08
@@ -14,7 +12,11 @@ import java.net.URL;
  */
 public class FileReader {
     /**
-     *
+     * 字符串解析分隔符
+     */
+    private static final String splitStr = ",";
+    /**
+     *饿
      * @param filePath 文件路径
      * @param flag 是否读取本地文件 true 本地文件 false 远程文件
      */
@@ -38,26 +40,52 @@ public class FileReader {
                 //进行数据处理
                 LogTemplate.info("读取文件数据整行:", lineTxt);
                 LogTemplate.info("读取文件数据分解:",null);
-                String[] list = FileParse.praseFile(lineTxt, null);
+                String[] list = FileParse.praseFile(lineTxt, splitStr);
                 for (Integer index = 0; index < list.length; index++){
                     if (list[index] != null){
                         LogTemplate.info("index" + index, list[index]);
                     }
                 }
             }
+            //关闭文件
+            bufferedReaderIn.close();
+        } catch (Exception e){
+            LogTemplate.info("etc文件读取失败", e.toString());
+            //关闭文件
+        }
+    }
+
+
+    /**
+     * 将数据集写入txt文件,并上传到ftp指定路径
+     * @param ftpPath ftp文件路径
+     * @param resultList  数据集合
+     */
+    public static void wirteTxtFile(final String ftpPath, final List<String> resultList){
+        String encoding = "utf8";
+        BufferedWriter bufferedWriterOut = null;
+        String filePath = "../test.txt";
+        //判断标志是否为true,true则为本地文件，false为远程地址文件
+        try {
+                File file = new File(filePath);
+                bufferedWriterOut = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding));
+            //遍历写入文件数据
+            for (Integer index = 0; index < resultList.size(); index++){
+                LogTemplate.info("操作文件数据整行:", index.toString());
+                LogTemplate.info("读取文件数据:", resultList.get(index));
+                bufferedWriterOut.write(resultList.get(index));
+            }
+             //读取文件缓存信息并进行解析
+             bufferedWriterOut.close();
+
+            //将本地文件上传到ftp文件服务器
         } catch (Exception e){
             LogTemplate.info("etc文件读取失败", e.toString());
         }
     }
 
     public static void main( String argv[]){
-        String filePath = "D:\\test.txt";
-        readTxtFile(filePath,Boolean.TRUE);
-
-//        String filePath = "ftp://@192.168.1.118/document/test.txt";
-////        ftp://wangxl:1234@172.16.31.69/aaa.txt
-//        readTxtFile(filePath,Boolean.FALSE);
-
-
+        String filePath = "ftp://root:qkjr_420@192.168.1.118:21/doc/test.txt";
+        readTxtFile(filePath,Boolean.FALSE);
     }
 }
