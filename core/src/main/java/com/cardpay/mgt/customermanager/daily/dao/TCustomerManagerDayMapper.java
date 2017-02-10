4fe3f0a291680e8473cab0d6a27864facef9ff68
yanweichen <505717760@qq.com>
2017-02-10 18:00:26 +0800

    feat:完成客户经理日报

--- core/src/main/java/com/cardpay/mgt/customermanager/daily/dao/TCustomerManagerDayMapper.java
+++ core/src/main/java/com/cardpay/mgt/customermanager/daily/dao/TCustomerManagerDayMapper.java
@@ -2,10 +2,24 @@ package com.cardpay.mgt.customermanager.daily.dao;
 
 import com.cardpay.basic.base.mapper.BasicMapper;
 import com.cardpay.mgt.customermanager.daily.model.TCustomerManagerDay;
+import com.cardpay.mgt.customermanager.daily.model.TCustomerManagerDayAndUser;
+import com.cardpay.mgt.customermanager.daily.model.vo.TCustomerManagerDayAndUserVo;
 
-/**
- * 客户经理日报信息Mapper
- * @author yanweichen
- */
+import java.util.List;
+import java.util.Map;
+
 public interface TCustomerManagerDayMapper extends BasicMapper<TCustomerManagerDay> {
+
+    /**
+     * 初始化所有客户经理日报
+     */
+    void initDaily();
+
+    /**
+     * 按条件查询日报
+     *
+     * @param map 查询条件
+     * @return 日报列表
+     */
+    List<TCustomerManagerDayAndUser> selectDailyAndUser(Map<String,Object> map);
 }
--- core/src/main/java/com/cardpay/mgt/customermanager/daily/service/CustomerManagerDayService.java
+++ core/src/main/java/com/cardpay/mgt/customermanager/daily/service/CustomerManagerDayService.java
@@ -2,6 +2,13 @@ package com.cardpay.mgt.customermanager.daily.service;
 
 import com.cardpay.basic.base.service.BaseService;
 import com.cardpay.mgt.customermanager.daily.model.TCustomerManagerDay;
+import com.cardpay.mgt.customermanager.daily.model.TCustomerManagerDayAndUser;
+import com.cardpay.mgt.customermanager.daily.model.vo.TCustomerManagerDayAndUserVo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 客户经理日报类
@@ -9,4 +16,33 @@ import com.cardpay.mgt.customermanager.daily.model.TCustomerManagerDay;
  */
 public interface CustomerManagerDayService extends BaseService<TCustomerManagerDay> {
 
+    /**
+     * 初始化所有客户经理日报
+     */
+    void initDaily();
+
+    /**
+     * 按条件查询日报返回vo
+     *
+     * @param map 查询条件
+     * @return 日报列表
+     */
+    List<TCustomerManagerDayAndUserVo> selectDailyAndUserVo(Map<String,Object> map);
+
+    /**
+     * 按条件查询日报返回
+     *
+     * @param map 查询条件
+     * @return 日报列表
+     */
+    List<TCustomerManagerDayAndUser> selectDailyAndUser(Map<String,Object> map);
+
+    /**
+     * 导出excel
+     *
+     * @param request request
+     * @param response response
+     * @return excel
+     */
+    void expertExcel(HttpServletRequest request, HttpServletResponse response);
 }
